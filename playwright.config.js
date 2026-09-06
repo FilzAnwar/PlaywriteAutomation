3d2a27846a8e4a6de081ff449684dcbab862013c
FilzAnwar <filzaanwar098@gmail.com>
2026-09-07 04:42:44 +0500

    Add Playwright HTML reporting

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     [
       "html",
       {
-        outputFolder: "Reporting",
+        outputFolder: "playwright-report",
         open: "never",
       },
     ],
